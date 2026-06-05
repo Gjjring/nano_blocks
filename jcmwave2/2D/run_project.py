0e609ab31978de64d5442eac2158c1d01530ffbe
phillip-manley <phillip.manley@jcmwave.com>
2026-06-05 13:52:18 +0200

    updated project to calculate transmission into the 3 outgoing ports

--- jcmwave2/2D/run_project.py
+++ jcmwave2/2D/run_project.py
@@ -1,5 +1,6 @@
 import jcmwave
 import os 
+import numpy as np
 keys = {}
 
 keys['cd_width'] = 6
@@ -7,7 +8,7 @@ keys['cd_height'] = 5
 keys['wg_width'] = 0.3
 keys['wg_displacement_left'] = -1
 keys['wg_displacement_right'] = 1
-keys['wg_stub_length'] = 1.
+keys['wg_stub_length'] = 0.75
 keys['boundary_id'] = 1
 keys['vacuum_wavelength'] = 500e-9
 keys['polygons'] = []
@@ -22,4 +23,8 @@ physical area of the user defined geometry is 1.0 to 4.0 micrometers high and
 keys['in_port_fields_path'] = os.path.join("..", "1D", "project_results", "fieldbag.jcm")
 jcmwave.geo(".", keys=keys)
 jcmwave.solve(os.path.join("..", "1D", "project.jcmpt"), keys=keys)
-jcmwave.solve("project.jcmpt", keys=keys)
+results =jcmwave.solve("project.jcmpt", keys=keys)
+
+print("upper left: {}".format(np.abs(results[3]['ModeCoefficients_Port1_Source'][0][0])))
+print("upper right: {}".format(np.abs(results[3]['ModeCoefficients_Port3_Source'][0][0])))
+print("lower right: {}".format(np.abs(results[3]['ModeCoefficients_Port2_Source'][0][0])))
